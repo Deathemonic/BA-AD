233d2d4d3444f9f06a2fb3354134b710dc060f9f
Deathemonic <zinth.chan@tuta.io>
2026-02-10 21:27:32 +0800

    chore: formatting

--- crates/baad/src/lib.rs
+++ crates/baad/src/lib.rs
@@ -4,6 +4,14 @@ pub mod cdn;
 pub mod download;
 pub mod strategy;
 
-pub use baad_core::{BuildType, DownloadObserver, Platform, set_observer};
+pub use baad_core::{
+    BuildType,
+    DownloadEvent,
+    DownloadObserver,
+    DownloadStatus,
+    NoopObserver,
+    Platform,
+    set_observer
+};
 pub use baad_utils::config::{LoggingConfig, init_logging};
 pub use baad_utils::{debug, error, file, info, trace, warn};
